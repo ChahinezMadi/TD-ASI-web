8b1ec793109bc15ad9c7c5fe7e17443a270c7f21
ChahinezMadi <chahinez.madi@etud.u-picardie.fr>
2025-12-16 18:34:38 +0100

    exercice-03 et gestion des erreurs

--- src/presentation/components/forms/ParcoursForm.vue
+++ src/presentation/components/forms/ParcoursForm.vue
@@ -5,6 +5,7 @@ import { Parcours } from '@/domain/entities/Parcours';
 import CustomInput from '@/presentation/components/forms/components/CustomInput.vue';
 import CustomButton from '@/presentation/components/forms/components/CustomButton.vue';
 import { ParcoursDAO } from '@/domain/daos/ParcoursDAO'; 
+import { watch } from 'vue';
 
 const currentParcours = ref<Parcours>(new Parcours(null, null, null));
 const isOpen = ref(false);
@@ -30,6 +31,64 @@ const props = defineProps({
     },
 });
 
+const formErrors = ref<{ 
+
+  NomParcours: string | null; 
+
+  AnneeFormation: string | null; 
+
+}>({ 
+
+  NomParcours: null, 
+
+  AnneeFormation: null, 
+
+}); 
+
+watch(() => currentParcours.value.NomParcours, () => { 
+
+  if (!currentParcours.value.NomParcours || currentParcours.value.NomParcours.trim() === '' || currentParcours.value.NomParcours.length < 3) { 
+
+    formErrors.value.NomParcours = 'Le nom du parcours doit faire au moins 3 caractères'; 
+
+  } else { 
+
+    formErrors.value.NomParcours = null; 
+
+  } 
+
+}); 
+
+watch(() => currentParcours.value.AnneeFormation, () => {
+  const raw = currentParcours.value.AnneeFormation;
+
+  //champ vide
+  if (raw === null || raw === undefined || raw === '') {
+    formErrors.value.AnneeFormation = "L'année de formation est obligatoire";
+    return;
+  }
+
+  const year = Number(raw);
+  const currentYear = new Date().getFullYear();
+  const min = currentYear - 20; // ou 2000
+  const max = currentYear + 20; // ou 2100
+
+  //pas un nombre ou pas un entier
+  if (!Number.isInteger(year)) {
+    formErrors.value.AnneeFormation = "L'année doit être un nombre entier";
+    return;
+  }
+
+  if (year < min || year > max) {
+    formErrors.value.AnneeFormation =
+      `L'année doit être comprise entre ${min} et ${max}`;
+    return;
+  }
+
+  // OK
+  formErrors.value.AnneeFormation = null;
+});
+
 onBeforeMount(() => {
     if (props.parcours) {
         currentParcours.value = props.parcours;
@@ -42,17 +101,27 @@ defineExpose({
 });
 
 const saveParcours = () => { 
+  if (formErrors.value.NomParcours || formErrors.value.AnneeFormation) { 
+    return; 
+  } 
   if (currentParcours.value.ID) { 
     // Mise à jour d'un parcours 
   } else { 
+
     ParcoursDAO.getInstance().create(currentParcours.value).then(() => { 
+
       alert('Parcours créé avec succès'); 
+
       closeForm(); 
+
     }).catch((ex) => { 
+
       alert(ex.message); 
+
     }); 
   } 
 }; 
+
 </script>
 <template>
     <div v-if="isOpen" class="custom-modal">
@@ -64,9 +133,9 @@ const saveParcours = () => {
             <div class="card-body">
                 <div class="card-text mt-1 mb-1">
                     <form>
-                        <CustomInput id="intitule" libelle="Intitulé" type="text" placeholder="Intitulé du parcours" v-model="currentParcours.NomParcours" />
+                        <CustomInput id="intitule" libelle="Intitulé" type="text" placeholder="Intitulé du parcours" v-model="currentParcours.NomParcours" :error="formErrors.NomParcours" />
                         <CustomInput class="mt-2" id="annee" libelle="Année" type="number"
-                            placeholder="Année de formation" v-model="currentParcours.AnneeFormation" />
+                            placeholder="Année de formation" v-model="currentParcours.AnneeFormation" :error="formErrors.AnneeFormation" />
                     </form>
                 </div>
                 <CustomButton class="mt-1" style="margin-left: 5px" :color="BootstrapButtonEnum.danger"
--- src/presentation/components/forms/components/CustomInput.vue
+++ src/presentation/components/forms/components/CustomInput.vue
@@ -6,12 +6,26 @@ const props = defineProps<{
     libelle: string;
     type: string;
     placeholder: string | undefined;
+    error: string | undefined | null,
 }>();
 </script>
 
-<template>
-    <div class="form-group">
-        <label for="intitule">{{ libelle }} :</label>
-        <input v-model="model" class="form-control" :type="type" :id="id" :placeholder="placeholder" />
-    </div>
+<template> 
+
+  <div class="form-group"> 
+
+    <label for="intitule">{{ libelle }} :</label> 
+
+    <input v-model="model" :class="{ 'form-control': true, 'is-invalid': error }" :type="type" :id="id" 
+
+      :placeholder="placeholder" /> 
+
+    <div v-if="error" class="invalid-feedback"> 
+
+      {{ error }} 
+
+    </div> 
+
+  </div> 
+
 </template>
